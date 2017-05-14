6114788c6158d0017cb740e97d0af599807b80d4
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-14 19:45:31 +0700

    add HtmlExtentions

---
+++ AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Data/Migrations/201705141201583_addProcLop.cs
@@ -0,0 +1,31 @@
+﻿namespace AppQuanLyThuHocPhi.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class addProcLop : DbMigration
+    {
+        public override void Up()
+        {
+            CreateStoredProcedure("InsertLOP", n => new
+            {
+                TenLop = n.String(),
+                Err = n.String()
+
+            },
+            @"if(@TenLop in(Select [TenLop] from [LOP]))
+	 begin
+		 set @Err=N'Trùng giá trị trường tên lớp'
+		 return 
+	 end
+	 Insert into [LOP] values(@TenLop)
+	 set @Err=''"
+            );
+        }
+
+        public override void Down()
+        {
+            DropStoredProcedure("InsertLOP");
+        }
+    }
+}
---
+++ AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Web/Helpers/HtmlExtentions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppQuanLyThuHocPhi.Web.Helpers
+{
+    public static class HtmlExtentions
+    {
+        public static MvcHtmlString Table<T>(this HtmlHelper helper, IEnumerable<T> models, params string[] cssClasses)
+        {
+            TagBuilder table = new TagBuilder("table");
+            StringBuilder tableInnerHtml = new StringBuilder();
+            string[] propertyNames = typeof(T).GetProperties().Select(info => info.Name).ToArray();
+            foreach (string cssClass in cssClasses)
+            {
+                table.AddCssClass(cssClass);
+            }
+
+            TagBuilder tableHeaderRow = new TagBuilder("tr");
+            StringBuilder tableHeaderInnerHtml = new StringBuilder();
+            foreach (string propertyName in propertyNames)
+            {
+                TagBuilder tableData = new TagBuilder("th");
+                tableData.InnerHtml = propertyName;
+                tableHeaderInnerHtml.Append(tableData);
+            }
+            tableHeaderRow.InnerHtml = tableHeaderInnerHtml.ToString();
+            tableInnerHtml.Append(tableHeaderRow);
+
+            foreach (var model in models)
+            {
+                TagBuilder tableDataRow = new TagBuilder("tr");
+                StringBuilder tableDataRowInnerHtml = new StringBuilder();
+                foreach (string propertyName in propertyNames)
+                {
+                    TagBuilder tableData = new TagBuilder("td");
+                    tableData.InnerHtml = typeof(T).GetProperty(propertyName).GetValue(model).ToString();
+                    tableDataRowInnerHtml.Append(tableData);
+                }
+
+                tableDataRow.InnerHtml = tableDataRowInnerHtml.ToString();
+                tableInnerHtml.Append(tableDataRow);
+            }
+            table.InnerHtml = tableInnerHtml.ToString();
+
+            return new MvcHtmlString(table.ToString(TagRenderMode.Normal));
+        }
+    }
+}
