0599755cf07f3d29ef55540464e05159509121d3
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-14 10:17:13 +0700

    add MappingConfig

---
+++ AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Web/App_Start/MappingConfig.cs
@@ -0,0 +1,36 @@
+﻿using AppQuanLyThuHocPhi.Entities.Models;
+using AppQuanLyThuHocPhi.Web.Models.ViewModel;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppQuanLyThuHocPhi.Web.App_Start
+{
+    public class MappingConfig
+    {
+        private static IMapper _mapper = null;
+        public static IMapper Mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                    InitMap();
+
+                return _mapper;
+            }
+        }
+
+        public static void InitMap()
+        {
+            var config = new MapperConfiguration(map =>
+            {
+                map.CreateMap<Lop, LopViewModel>();
+               
+            });
+
+            _mapper = config.CreateMapper();
+        }
+    }
+}
--- AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Web/Global.asax.cs
+++ AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppQuanLyThuHocPhi.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,10 +13,12 @@ namespace AppQuanLyThuHocPhi.Web
     {
         protected void Application_Start()
         {
+            IoCContainer.InitContainer();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MappingConfig.InitMap();
         }
     }
 }
