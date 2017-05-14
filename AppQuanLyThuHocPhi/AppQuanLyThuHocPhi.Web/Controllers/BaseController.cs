67d9c5e4da94e8056d17ea4e85eabe6f52e25792
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-14 12:53:10 +0700

    add base

---
+++ AppQuanLyThuHocPhi/AppQuanLyThuHocPhi.Web/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppQuanLyThuHocPhi.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected static readonly NLog.ILogger _log = NLog.LogManager.GetCurrentClassLogger();
+        protected List<string> ModelErrors
+        {
+            get
+            {
+                if (ModelState.IsValid == false)
+                    return new List<string>();
+
+                return ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+            }
+        }
+
+        protected void Alert(string message, bool isErorr = false)
+        {
+            if (isErorr)
+                TempData["error"] = message;
+            else
+                TempData["success"] = message;
+        }
+    }
+}
